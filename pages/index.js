1b2cf1643a05091c8f5e20f029d081d1edfc6243
kkssuvro <ksshorts2m@gmail.com>
2024-12-04 17:34:35 +0600

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,1 +1,21 @@
+// pages/redirect.js (for Next.js or similar frameworks)
+import { useEffect } from 'react';
+
+const RedirectPage = () => {
+  useEffect(() => {
+    // Set a delay (e.g., 5000ms = 5 seconds)
+    setTimeout(() => {
+      // Redirect to your desired URL after the delay
+      window.location.href = 'https://exmpire.online/RSSNEW.html';
+    }, 5000); // 5000ms = 5 seconds
+  }, []);
+
+  return (
+    <div>
+      <p>You will be redirected shortly...</p>
+    </div>
+  );
+};
+
+export default RedirectPage;
 
